1b0645e57e31ba9938730d87f7cafc7a9d0c1a17
Heemo Yang <heemo521@gmail.com>
2022-10-10 02:56:28 -0700

    Add fetching on exploremain

--- client/src/features/Explore/ExploreMain.tsx
+++ client/src/features/Explore/ExploreMain.tsx
@@ -1,5 +1,5 @@
-import React, {FunctionComponent} from 'react'
-
+import React, {FunctionComponent, useEffect, useState} from 'react'
+import axios from 'axios'
 export interface IExploreMainProps {
   city: string | null
   c: string
@@ -8,6 +8,22 @@ export interface IExploreMainProps {
 }
 
 const ExploreMain: FunctionComponent<IExploreMainProps> = ({city, c, t, p}) => {
+  const [eventList, setEventList] = useState([])
+  const [coords, setCoords] = useState([] as number[])
+  useEffect(() => {
+    if (city === 'near') {
+      // TODO: get coordinates and set coords
+    }
+    void getCityEvents()
+  }, [])
+
+  const getCityEvents = async () => {
+    const [lat, lng] = coords
+
+    console.log('hello getting city events')
+
+    axios(`http://localhost:5000/v1/events/?category=popular&t=${t}&p=${p}&city=${city}&lat=${lat}&lng=${lng}`)
+  }
   return (
     <div style={{color: 'white', fontSize: '2rem'}}>
       <div>Explore {city}</div>
--- client/src/pages/Explore.tsx
+++ client/src/pages/Explore.tsx
@@ -5,17 +5,15 @@ import {useSearchParams} from 'react-router-dom'
 
 const Explore: FunctionComponent = () => {
   const [searchParams, setSearchParams] = useSearchParams()
-  //posh.vip/explore?c=popular&t=week&p=1&city=
-  //posh.vip/explore?c=popular&t=week&p=1&city=la
+
   const city = searchParams.get('city') || null
-  const c = 'popular' || searchParams.get('c')
-  const t = searchParams.get('t') || 'week'
+  const c = searchParams.get('c') === 'popular' ? 'popular' : 'popular'
+  const t = searchParams.get('t') === 'day' ? 'day' : 'week'
   const p = Number(searchParams.get('p')) || 1
 
   useEffect(() => {
     if (!city) {
       setSearchParams('?c=popular&t=week&p=1&city=')
-      //navigate to CitySelector Page
     } else {
       setSearchParams(`?c=${c}&t=${t}&p=${p}&city=${city}`)
     }
