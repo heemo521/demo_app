ded474f64986310fe1b0fa14c079404fb338af9f
Heemo Yang <heemo521@gmail.com>
2022-10-09 22:44:26 -0700

    Set up city selector page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import CitySelector from 'pages/CitySelector'
+import React, {FunctionComponent} from 'react'
+import {Route} from 'react-router'
+import {BrowserRouter, Routes} from 'react-router-dom'
 
 import './assets/stylesheets/styles.scss'
-import '../src/assets/webfonts/Nunito/Nunito-Black.ttf'
+// import '../src/assets/webfonts/Nunito/Nunito-Black.ttf'
 
-const App = () => {
-  return <p style={{color: 'white'}}>Good Luck!</p>
+export interface IAppProps {}
+
+const App: FunctionComponent<IAppProps> = props => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<CitySelector {...props} />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
---
+++ client/src/pages/CitySelector.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+interface ICitySelectorProps {}
+
+const CitySelector: React.FunctionComponent<ICitySelectorProps> = props => {
+  return <div className='CitySelector'>CitySelector</div>
+}
+
+export default CitySelector
