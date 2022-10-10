ee11b73a3c5078ad0128849211af15d06f2ab59e
Heemo Yang <heemo521@gmail.com>
2022-10-10 00:00:12 -0700

    Set up useEffect to fetch dynamic city list (needs fetching)

--- client/src/features/CitySelector/CitySelectorBody.tsx
+++ client/src/features/CitySelector/CitySelectorBody.tsx
@@ -1,16 +1,25 @@
-import * as React from 'react'
+import React, {FunctionComponent, useEffect, useState} from 'react'
 
 interface ICitySelectorBodyProps {}
 
 const CitySelectorBody: React.FunctionComponent<ICitySelectorBodyProps> = props => {
+  const [citiesList, setCitiesList] = useState([] as string[])
+  const [selectedCity, setSelectedCity] = useState('')
+
+  useEffect(() => {
+    const cities = ['new york', 'miami', 'los angeles', 'near me']
+    setCitiesList(cities)
+  }, [])
+
   return (
     <div className='CitySelector-Body'>
       <div>Where are you looking for experiences?</div>
       <li>
-        <ul>City</ul>
-        <ul>City</ul>
-        <ul>City</ul>
-        <ul>City</ul>
+        {citiesList.map((city: string, i: number) => (
+          <ul className='city_city' key={i}>
+            {city}
+          </ul>
+        ))}
       </li>
     </div>
   )
