1c95e5af20fb767f23a524109472c05bf8d90ef9
Heemo Yang <heemo521@gmail.com>
2022-10-10 04:35:55 -0700

    Set up server and folder sctructure

--- app.ts
+++ app.ts
@@ -1,12 +1,19 @@
 import express from 'express'
 
+import {json, urlencoded} from 'body-parser'
 import cors from 'cors'
+
 const app = express()
 app.use(cors())
 
 // GET /
-app.get('/', (req, res) => {
+//localhost:5000/v1/events/?category=popular&t=${t}&p=${p}&city=${city}&lat=${lat}&lng=${lng}
+app.get('/v1/events', (req, res) => {
   res.send('Hello World!!!')
 })
+
+app.use(cors())
+app.use(json())
+app.use(urlencoded({extended: true}))
 
 export default app
