cf44c2124914f48096b12e7090730b0dbdf193e3
Heemo Yang <heemo521@gmail.com>
2022-10-10 03:33:31 -0700

    Create explore body

---
+++ client/src/features/Explore/ExploreBody.tsx
@@ -0,0 +1,11 @@
+import React, {FunctionComponent} from 'react'
+
+const ExploreBody: FunctionComponent = () => {
+  return (
+    <div className='Explore-body'>
+      <div className='Explore-body-main'></div>
+    </div>
+  )
+}
+
+export default ExploreBody
--- client/src/features/Explore/ExploreCover.tsx
+++ client/src/features/Explore/ExploreCover.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, {FunctionComponent} from 'react'
 
-export default function ExploreCover({}) {
+// const ExploreCover: FunctionComponent = () => {
+//   return (
+//     <div className='Explore-cover'>
+//       <video autoPlay loop muted className='Explore-cover-video'>
+//         <source src='https://posh-b2.s3.us-east-2.amazonaws.com/meta+(1)_1.mp4' type='video/mp4' />
+//       </video>
+//     </div>
+//   )
+// }
+
+const ExploreVideos: FunctionComponent = () => {
+  // Can I render use the same video twice ?
   return (
-    <div className='Explore-cover'>
-      <video autoPlay loop muted className='Explore-cover-video'>
+    <>
+      <video autoPlay loop muted className='Explore-video'>
         <source src='https://posh-b2.s3.us-east-2.amazonaws.com/meta+(1)_1.mp4' type='video/mp4' />
       </video>
-    </div>
+      <div className='Explore-cover'>
+        <video autoPlay loop muted className='Explore-cover-video'>
+          <source src='https://posh-b2.s3.us-east-2.amazonaws.com/meta+(1)_1.mp4' type='video/mp4' />
+        </video>
+      </div>
+    </>
   )
 }
+
+export default ExploreVideos
