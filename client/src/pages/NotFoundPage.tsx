891d8ccda8205a70cf798e18cd0c0004fe6d7f6f
Heemo Yang <heemo521@gmail.com>
2022-10-10 20:58:20 -0700

    Style 404 page and redirect timer

--- client/src/pages/NotFoundPage.tsx
+++ client/src/pages/NotFoundPage.tsx
@@ -1,14 +1,21 @@
-import React, {FunctionComponent, useEffect} from 'react'
+import React, {FunctionComponent, useEffect, useState} from 'react'
 import {Link} from 'react-router-dom'
 
 type Props = {}
 
 const NotFoundPage: FunctionComponent = (props: Props) => {
+  const [redirect, seRedirect] = useState(false)
+  useEffect(() => {
+    document.title = '404! Page Not Found'
+    setTimeout(() => window.location.assign('/explore'), 5000)
+  }, [])
+
   return (
-    <h2>
-      There was an error with this listing. <Link to='/explore'>Click here</Link> to back to the home page or wait five
+    <div className='Not_Found'>
+      <h1>404! PAGE NOT FOUND</h1>
+      There was an error with this page. <Link to='/explore'>Click here</Link> to back to the home page or wait five
       seconds.
-    </h2>
+    </div>
   )
 }
 
